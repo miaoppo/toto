20ffa4c5e8c6f55dcbc259bf6787c28f7f73310f
bpkddr <53825262+bpkddr@users.noreply.github.com>
2023-01-25 19:32:49 +0700

    ...

--- app.py
+++ app.py
@@ -967,8 +967,8 @@ class Session:
 		return ugent		
 		
 if __name__=="__main__":
-	# try:os.system("git pull")
-	# except:pass
+	try:os.system("git pull")
+	except:pass
 	try:os.mkdir("OK")
 	except:pass
 	try:os.mkdir("CP")
